805212c2b052a62e0917910ff864d7e368740a09
kyle <shockey@users.noreply.github.com>
2018-09-24 21:38:02 -0500

    update 03-code-examples-after-responses.js

--- 03-code-examples-after-responses.js
+++ 03-code-examples-after-responses.js
@@ -3,9 +3,9 @@
 const CodeExamplesAfterResponsesPlugin = () => {
   return {
     wrapComponents: {
-      responses: (Responses, { React }) => (props) => {
+      responses: (OriginalComponent, { React }) => (props) => {
         return <div>
-          <Responses {...props} />
+          <OriginalComponent {...props} />
           <iframe src="https://docs.cloudmersive.com" width="100%" height="30px" />
         </div>
       }
